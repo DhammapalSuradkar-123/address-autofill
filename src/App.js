075076f22b1af5f7815863bd9be668659c4b24d2
dhammapal suradkar <dhammapal.suradkar@commercepundit.com>
2025-08-01 19:44:10 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AddressAutofill from "./pages/AddressAutofill";
+// import ReactSlick from "./pages/ReactSlick";
+// import Swiper from "./pages/Swiper";
+// import VideoPlayer from "./pages/VideoPlayer";
+
+function App() {
+  return (
+    <>
+      {/* <ReactSlick /> */}
+      {/* <Swiper /> */}
+      {/* <VideoPlayer /> */}
+      <AddressAutofill />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/AddressAutofill.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+
+const AddressForm = () => {
+  const [formData, setFormData] = useState({
+    addressOne: '',
+    city: '',
+    state: '',
+    zip: '',
+    country: '',
+  });
+
+  const handleChange = (name, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted address:', formData);
+    // you can handle API or state update here
+  };
+
+  return (
+    <main>
+      <div className="wrapper" style={{margin: "100px"}}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="address-line1" style={{display:"inline-block",width:"300px"}}>Address line</label>
+            <input
+              autoComplete="address-line1"
+              required
+              type="text"
+              id="address-line1"
+              name="address-line1"
+              value={formData.addressOne}
+              onChange={(e) => handleChange("addressOne", e.target.value)}
+            />
+          </div>
+          <br/>
+          <div>
+            <label htmlFor="address-level2" style={{display:"inline-block",width:"300px"}}>City</label>
+            <input
+              autoComplete="address-level2"
+              required
+              type="text"
+              id="address-level2"
+              name="address-level2"
+              value={formData.city}
+              onChange={(e) => handleChange("city", e.target.value)}
+            />
+          </div>
+          <br/>
+          <div>
+            <label htmlFor="address-level1" style={{display:"inline-block",width:"300px"}}>State / Province / Region (optional)</label>
+            <input
+              autoComplete="address-level1"
+              type="text"
+              id="address-level1"
+              name="address-level1"
+              value={formData.state}
+              onChange={(e) => handleChange("state", e.target.value)}
+            />
+          </div>
+          <br/>
+          <div>
+            <label htmlFor="postal-code" style={{display:"inline-block",width:"300px"}}>ZIP / Postal code (optional)</label>
+            <input
+              autoComplete="postal-code"
+              type="text"
+              id="postal-code"
+              name="postal-code"
+              value={formData.zip}
+              onChange={(e) => handleChange("zip", e.target.value)}
+            />
+          </div>
+          <br/>
+          <div>
+            <label htmlFor="country" style={{display:"inline-block",width:"300px"}}>Country</label>
+            <input
+              autoComplete="country"
+              type="text"
+              id="country"
+              name="country"
+              value={formData.country}
+              onChange={(e) => handleChange("country", e.target.value)}
+            />
+          </div>
+          <br/><br/>
+          <div style={{display:"flex"}}>
+            <div style={{width: "300px"}} />
+            <button type="submit">Save address</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default AddressForm;
