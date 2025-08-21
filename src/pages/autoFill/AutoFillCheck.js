90798c571eebdb3acfa55bce12ecdcf07c7c6aff
dhammapal suradkar <dhammapal.suradkar@commercepundit.com>
2025-08-21 18:13:18 +0530

    changes

--- src/pages/autoFill/AutoFillCheck.js
+++ src/pages/autoFill/AutoFillCheck.js
@@ -18,143 +18,94 @@ const AutoFillCheck = () => {
     return (
         <form autoComplete="on">
           <div>
-            <div>
-              <input
-              label="First Name*"
-              type="text"
-              name="given-name"
-              id="given-name"
-              autoComplete="given-name"
-              placeholder="First Name*"
-              value={address.firstname}
-              onChange={(e) => {
-                handleOnAddressChange('firstname', e.target.value?.trim?.());
-              }}
+          <input
+            type="text"
+            name="given-name"
+            id="given-name"
+            autoComplete="given-name"
+            placeholder="First Name*"
+            value={address.firstname || ""}
+            onChange={(e) => handleOnAddressChange('firstname', e.target.value?.trim?.())}
             />
-            </div>
 
-            <div>
-              <input
-                label="Last Name*"
-                type="text"
-                name="family-name"
-                id="family-name"
-                autoComplete="family-name"
-                placeholder="Last Name*"
-                value={address.lastname}
-                onChange={(e) => {
-                  handleOnAddressChange('lastname', e.target.value?.trim?.());
-                }}
-                onBlur={(e) => {
-                  handleOnAddressChange('lastname', e.target.value?.trim?.());
-                }}
-                className="formField"
-              />
-            </div>
-          </div>
-          <div className={`formGroup`}>
-            <div
+            <input
+            type="text"
+            name="family-name"
+            id="family-name"
+            autoComplete="family-name"
+            placeholder="Last Name*"
+            value={address.lastname || ""}
+            onChange={(e) => handleOnAddressChange('lastname', e.target.value?.trim?.())}
+            />
+
+            <input
+            type="tel"
+            name="tel"
+            id="tel"
+            autoComplete="tel"
+            placeholder="Mobile Number*"
+            value={address.telephone || ""}
+            onChange={(e) => handleOnAddressChange('telephone', e.target.value?.trim?.())}
+            />
+
+            <input
+            type="text"
+            name="address-line1"
+            id="address-line1"
+            autoComplete="address-line1"
+            placeholder="Street/Road"
+            value={address.street || ""}
+            onChange={(e) => handleOnAddressChange('street', e.target.value)}
+            />
+
+            <input
+            type="text"
+            name="postal-code"
+            id="postal-code"
+            autoComplete="postal-code"
+            placeholder="Zipcode*"
+            value={address.postcode || ""}
+            onChange={(e) => handleOnAddressChange('postcode', e.target.value?.trim?.())}
+            />
+
+            <input
+            type="text"
+            name="address-level2"
+            id="address-level2"
+            autoComplete="address-level2"
+            placeholder="City*"
+            value={address.city || ""}
+            onChange={(e) => handleOnAddressChange('city', e.target.value?.trim?.())}
+            />
+
+            <select
+            id="address-level1"
+            name="address-level1"
+            autoComplete="address-level1"
+            value={address.state || ""}
+            onChange={(e) => handleOnAddressChange("state", e.target.value)}
             >
-              <input
-                label="Mobile Number*"
-                type="text"
-                name="tel"
-                id="tel"
-                autoComplete="tel"
-                placeholder="Mobile Number*"
-                value={address.telephone}
-                onChange={(e) => {
-                  handleOnAddressChange('telephone', e.target.value?.trim?.());
-                  handleOnAddressChange('cell_phone', e.target.value?.trim?.());
-                }}
-                className="formField"
-              />
-            </div>
-          </div>
-          <div className="formGroup fullWidthForm field">
-            <input
-              label="Company Name"
-              type="text"
-              placeholder="Company Name"
-              name="organization"
-              id="organization"
-              autoComplete="organization"
-              value={address.company}
-              onChange={(e) => {
-                handleOnAddressChange('company', e.target.value?.trim?.());
-              }}
-            />
-          </div>
+            <option value="">Select State</option>
+            <option value="CA">California</option>
+            <option value="NY">New York</option>
+            <option value="GA">Georgia</option>
+            <option value="MH">Maharashtra</option>
+            </select>
 
-          <div className={`formGroup fullWidthForm field`}>
-            <input
-              label="Street/Road"
-              type="text"
-              name="address-line1"
-              id="address-line1"
-              autoComplete="address-line1"
-              placeholder="Street/Road"
-              value={address?.street || ''}
-              onChange={(e) => {
-                handleOnAddressChange('street', e.target.value);
-              }}
-              className="formField"
-            />
-          </div>
-          <div className={`formGroup`}>
-            <div>
-              <input
-                type="text"
-                name="postal-code"
-                id="postal-code"
-                autoComplete="postal-code"
-                placeholder="Zipcode*"
-                value={address.postcode}
-                onChange={(e) => {
-                  handleOnAddressChange('postcode', e.target.value?.trim?.());
-                }}
-                className="formField"
-              />
-            </div>
-
-            <div>
-              <input
-                label="City*"
-                type="text"
-                placeholder="City*"
-                value={address.city}
-                name="address-level2"
-                id="address-level2"
-                autoComplete="address-level2"
-                onChange={(e) => {
-                  handleOnAddressChange('city', e.target.value?.trim?.());
-                }}
-              />
-            </div>
-          </div>
-          <div className={`formGroup`}>
-            <div>
-                <select id="address-level1" name="address-level1" autoComplete="address-level1" value={address.state || ""} onChange={(e) =>handleOnAddressChange("state", e.target.value)}>
-                    <option value="CA">California</option>
-                    <option value="NY">New York</option>
-                    <option value="GA">Gorgia</option>
-                    <option value="MH">Maharashtra</option>
-                </select>
-                <label className="formLabel">State*</label>
-            </div>
-            <div>
-                <select id="country" name="country" autoComplete="country" value={address.country || ""} onChange={(e) =>handleOnAddressChange("country", e.target.value)}>
-                    <option value="US">United States</option>
-                    <option value="IN">India</option>
-                    <option value="CA">Canada</option>
-                    <option value="AU">Austrelia</option>
-                </select>
-                <label className="formLabel">Country*</label>
-            </div>
-          </div>
-          <div className="clearfix" />
-            <div className="btnSet">
-              <button
+            <select
+            id="country"
+            name="country"
+            autoComplete="country"
+            value={address.country || ""}
+            onChange={(e) => handleOnAddressChange("country", e.target.value)}
+            >
+            <option value="">Select Country</option>
+            <option value="US">United States</option>
+            <option value="IN">India</option>
+            <option value="CA">Canada</option>
+            <option value="AU">Australia</option>
+            </select>
+            <button
                 type="submit"
                 className="btn btnPrimary"
                 onClick={(event) => {
@@ -164,7 +115,8 @@ const AutoFillCheck = () => {
               >
                Save
               </button>
-            </div>
+
+          </div>
         </form>
     )
 }
